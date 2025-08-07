81d944cf5d61473ad123691d93781dc70811bf6b
Skyerv <amanda@manzoti.com>
2025-08-07 13:38:14 -0300

    feat: configure material, assets, and initial routes

---
+++ src/app/shared/material-shared.module.ts
@@ -0,0 +1,55 @@
+
+import { NgModule } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+@NgModule({
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatCheckbox,
+    MatDividerModule,
+    MatSnackBarModule,
+  ],
+  exports: [
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatToolbarModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatCheckbox,
+    MatDividerModule,
+    MatSnackBarModule,
+  ],
+})
+export class MaterialSharedModule {}
