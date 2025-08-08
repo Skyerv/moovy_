1347cd4c7ed681a4cca8aa954bf8ccc60e9ef76b
Skyerv <amanda@manzoti.com>
2025-08-08 20:25:15 -0300

    revert: testing if lazy loading is breaking deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
+import { MovieDetailsDialog } from 'movies/components/movie-details-dialog/movie-details-dialog';
+import { MoviesHub } from 'movies/pages/movies-hub/movies-hub';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('movies/movies-module').then(m => m.MoviesModule),
+    component: MoviesHub,
+    children: [{ path: 'movies/:id/details', component: MovieDetailsDialog }],
   },
   {
     path: 'movies',
-    loadChildren: () =>
-      import('movies/movies-module').then(m => m.MoviesModule),
+    component: MoviesHub,
+    children: [{ path: 'movies/:id/details', component: MovieDetailsDialog }],
   },
 ];
--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -1,33 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesHub } from './pages/movies-hub/movies-hub';
+import { MovieDetailsDialog } from './components/movie-details-dialog/movie-details-dialog';
 
 const routes: Routes = [
   {
     path: '',
     component: MoviesHub,
-    children: [
-      {
-        path: 'movies/:id/details',
-        loadComponent: () =>
-          import(
-            'movies/components/movie-details-dialog/movie-details-dialog'
-          ).then((m) => m.MovieDetailsDialog),
-      },
-    ],
+    children: [{ path: 'movies/:id/details', component: MovieDetailsDialog }],
   },
   {
     path: 'movies',
     component: MoviesHub,
-    children: [
-      {
-        path: 'movies/:id/details',
-        loadComponent: () =>
-          import(
-            'movies/components/movie-details-dialog/movie-details-dialog'
-          ).then((m) => m.MovieDetailsDialog),
-      },
-    ],
+    children: [{ path: 'movies/:id/details', component: MovieDetailsDialog }],
   },
 ];
 
